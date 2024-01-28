e49dff4de930403f61b3cea8e9d322168cecd9d4
SidGames5 <saytleader@outlook.com>
2024-01-28 10:13:43 -0500

    Add more validation

--- src/net/PacketHandler.hx
+++ src/net/PacketHandler.hx
@@ -53,6 +53,21 @@ class PacketHandler {
 
             user.id = latestId + 1;
 
+            if (user.channels == null)
+                user.channels = new Array<Int>();
+            if (user.displayName == null)
+                user.displayName = user.username;
+            if (user.passwordHash == null) {
+                req.replyData("No password provided", "text/plain", 417);
+                return;
+            }
+            if (user.requests == null)
+                user.requests = new Array<Int>();
+            if (user.username == null) {
+                req.replyData("No username provided", "text/plain", 417);
+                return;
+            }
+
             DatabaseManager.addUser(user);
 
             // create a new session token
